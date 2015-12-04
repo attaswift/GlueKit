84100d056435fb697a4e7889c7d837cbb5a02fc1
Karoly Lorentey <karoly@lorentey.hu>
2015-12-04 17:59:44 +0100

    Add some more source operators: map, filter, flatMap, dispatch, everyNth. Fun!

--- GlueKit/SourceOperators.swift
+++ GlueKit/SourceOperators.swift
@@ -13,5 +13,63 @@ extension SourceType {
         return Source<Output> { sink in self.connect { value in operation(value, sink) } }
     }
 
-    // More to be defined later.
+    public func map<Output>(transform: Value->Output) -> Source<Output> {
+        return sourceOperator { input, sink in
+            sink(transform(input))
+        }
+    }
+
+    public func filter(predicate: Value->Bool) -> Source<Value> {
+        return sourceOperator { input, sink in
+            if predicate(input) {
+                sink(input)
+            }
+        }
+    }
+
+    public func flatMap<Output>(transform: Value->Output?) -> Source<Output> {
+        return sourceOperator { input, sink in
+            if let output = transform(input) {
+                sink(output)
+            }
+        }
+    }
+
+    public func flatMap<Output>(transform: Value->[Output]) -> Source<Output> {
+        return sourceOperator { input, sink in
+            for output in transform(input) {
+                sink(output)
+            }
+        }
+    }
+
+    public func dispatch(queue: dispatch_queue_t) -> Source<Value> {
+        return sourceOperator { input, sink in
+            dispatch_async(queue, { sink(input) })
+        }
+    }
+
+    public func dispatch(queue: NSOperationQueue) -> Source<Value> {
+        return sourceOperator { input, sink in
+            if NSOperationQueue.currentQueue() == queue {
+                sink(input)
+            }
+            else {
+                queue.addOperationWithBlock { sink(input) }
+            }
+        }
+    }
+
+    public func everyNth(n: Int) -> Source<Value> {
+        assert(n > 0)
+        return Source { sink in
+            var count = 0
+            return self.connect { value in
+                if ++count == n {
+                    count = 0
+                    sink(value)
+                }
+            }
+        }
+    }
 }
---
+++ GlueKitTests/SourceOperatorTests.swift
@@ -0,0 +1,187 @@
+//
+//  SourceOperatorTests.swift
+//  GlueKit
+//
+//  Created by Károly Lőrentey on 2015-12-03.
+//  Copyright © 2015 Károly Lőrentey. All rights reserved.
+//
+
+import XCTest
+import GlueKit
+
+class SourceOperatorTests: XCTestCase {
+
+    func testSourceOperatorIsCalledOnEachSinkDuringEachSend() {
+        let signal = Signal<Int>()
+
+        var count = 0
+        let source = signal.sourceOperator { (input: Int, sink: Double->()) in
+            count++
+        }
+        XCTAssertEqual(count, 0)
+
+        let c1 = source.connect { _ in }
+        let c2 = source.connect { _ in }
+        XCTAssertEqual(count, 0)
+
+        signal.send(10)
+        XCTAssertEqual(count, 2)
+
+        signal.send(20)
+        XCTAssertEqual(count, 4)
+
+        signal.send(30)
+        XCTAssertEqual(count, 6)
+
+        c1.disconnect()
+        c2.disconnect()
+
+        signal.send(40)
+        XCTAssertEqual(count, 6)
+    }
+
+    func testSourceOperatorRetainsSource() {
+        var source: Source<Int>? = nil
+        weak var weakSignal: Signal<Int>? = nil
+        do {
+            let signal = Signal<Int>()
+            weakSignal = signal
+
+            source = signal.sourceOperator { (input: Int, sink: Int->()) in
+                // Noop
+                sink(input)
+            }
+        }
+
+        XCTAssertNotNil(weakSignal)
+
+        source = nil
+        XCTAssertNil(weakSignal)
+
+        noop(source)
+    }
+
+    func testSourceDoesntRetainOperator() {
+        weak var weakResource: NSObject? = nil
+        do {
+            let resource = NSObject()
+            weakResource = resource
+            let source = Signal<Int>().sourceOperator { (input: Int, sink: Int->()) in
+                noop(resource)
+                sink(input)
+            }
+            XCTAssertNotNil(weakResource)
+            noop(source)
+        }
+
+        XCTAssertNil(weakResource)
+    }
+
+    func testMap() {
+        let signal = Signal<Int>()
+
+        let source = signal.map { i in "\(i)" }
+
+        var received = [String]()
+        let connection = source.connect { received.append("\($0)") }
+
+        signal.send(1)
+        signal.send(2)
+        signal.send(3)
+
+        connection.disconnect()
+
+        XCTAssertEqual(received, ["1", "2", "3"])
+    }
+
+    func testFilter() {
+        let signal = Signal<Int>()
+        let oddSource = signal.filter { $0 % 2 == 1 }
+
+        var received = [Int]()
+        let connection = oddSource.connect { received.append($0) }
+
+        (1...10).forEach { signal.send($0) }
+
+        connection.disconnect()
+
+        XCTAssertEqual(received, [1, 3, 5, 7, 9])
+    }
+
+    func testOptionalFlatMap() {
+        let signal = Signal<Int>()
+        let source = signal.flatMap { i in i % 2 == 0 ? i / 2 : nil }
+
+        var received = [Int]()
+        let connection = source.connect { received.append($0) }
+
+        (1...10).forEach { signal.send($0) }
+
+        connection.disconnect()
+
+        XCTAssertEqual(received, [1, 2, 3, 4, 5])
+    }
+
+    func testArrayFlatMap() {
+        let signal = Signal<Int>()
+        let source = signal.flatMap { (i: Int) -> [Int] in
+            if i > 0 {
+                return (1...i).filter { i % $0 == 0 }
+            }
+            else {
+                return []
+            }
+        }
+        // Source sends all divisors of all numbers sent by its input source.
+
+        var received = [Int]()
+        let connection = source.connect { received.append($0) }
+
+        (1...10).forEach { signal.send($0) }
+
+        connection.disconnect()
+
+        XCTAssertEqual(received, [
+            1,
+            1, 2,
+            1, 3,
+            1, 2, 4,
+            1, 5,
+            1, 2, 3, 6,
+            1, 7,
+            1, 2, 4, 8,
+            1, 3, 9,
+            1, 2, 5, 10
+        ])
+    }
+
+    func testEveryNth() {
+        let signal = Signal<Int>()
+        let source = signal.everyNth(3)
+
+        var r1 = [Int]()
+        let c1 = source.connect { r1.append($0) }
+
+        signal.send(1)
+
+        var r2 = [Int]()
+        let c2 = source.connect { r2.append($0) }
+
+        signal.send(2)
+
+        var r3 = [Int]()
+        let c3 = source.connect { r3.append($0) }
+
+        (3...11).forEach(signal.send)
+
+        c1.disconnect()
+        c2.disconnect()
+        c3.disconnect()
+
+        // Each sink gets its own counter.
+        
+        XCTAssertEqual(r1, [3, 6, 9])
+        XCTAssertEqual(r2, [4, 7, 10])
+        XCTAssertEqual(r3, [5, 8, 11])
+    }
+}
