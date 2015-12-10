478ffb56d6b7cff7beb3fb954217e33ac292f310
Karoly Lorentey <karoly@lorentey.hu>
2015-12-10 02:05:41 +0100

    Implement efficient change descs for Arrays
    ArrayChange supports merging multiple array operations into a single update.

--- Sources/ArrayVariable.swift
+++ Sources/ArrayVariable.swift
@@ -8,40 +8,194 @@
 
 import Foundation
 
+//MARK: ArrayModification
 
-public enum ArrayChange<Element>: ChangeType {
-    public typealias Value = [Element]
-
+internal enum ArrayModification<Element> {
     case Insert(Element, at: Int)
     case RemoveAt(Int)
     case ReplaceAt(Int, with: Element)
     case ReplaceRange(Range<Int>, with: [Element])
-    case ReplaceAll([Element])
 
-    public init(oldValue: Value, newValue: Value) {
-        self = .ReplaceAll(newValue)
+    var countDelta: Int {
+        switch self {
+        case .Insert(_, at: _): return 1
+        case .RemoveAt(_): return -1
+        case .ReplaceAt(_, with: _): return 0
+        case .ReplaceRange(let range, with: let es): return es.count - range.count
+        }
     }
 
-    public func applyOn(value: Value) -> Value {
-        var result = value
-        result.apply(self)
-        return result
+    func toRangeReplacement() -> (range: Range<Int>, elements: [Element]) {
+        switch self {
+        case .Insert(let e, at: let i):
+            return (Range(start: i, end: i), [e])
+        case .RemoveAt(let i):
+            return (Range(start: i, end: i + 1), [])
+        case .ReplaceAt(let i, with: let e):
+            return (Range(start: i, end: i + 1), [e])
+        case .ReplaceRange(let range, with: let es):
+            return (range, es)
+        }
+    }
+
+    func merge(mod: ArrayModification<Element>) -> ArrayModificationMergeResult<Element> {
+        let rm1 = self.toRangeReplacement()
+        let rm2 = mod.toRangeReplacement()
+
+        if rm1.range.startIndex + rm1.elements.count < rm2.range.startIndex {
+            // New range affects indices greater than our range
+            return .DisjunctOrderedAfter
+        }
+        else if rm2.range.endIndex < rm1.range.startIndex {
+            // New range affects indices earlier than our range
+            return .DisjunctOrderedBefore
+        }
+
+        // There is some overlap.
+        let delta = rm1.elements.count - rm1.range.count
+
+        let combinedRange = Range<Int>(
+            start: min(rm1.range.startIndex, rm2.range.startIndex),
+            end: max(rm1.range.endIndex, rm2.range.endIndex - delta)
+        )
+
+        let replacement = Range<Int>(
+            start: max(0, rm2.range.startIndex - rm1.range.startIndex),
+            end: min(rm1.elements.count, rm2.range.endIndex - rm1.range.startIndex))
+        var combinedElements = rm1.elements
+        combinedElements.replaceRange(replacement, with: rm2.elements)
+
+        switch (combinedRange.count, combinedElements.count) {
+        case (0, 0):
+            return .CollapsedToNoChange
+        case (0, 1):
+            return .CollapsedTo(.Insert(combinedElements[0], at: combinedRange.startIndex))
+        case (1, 0):
+            return .CollapsedTo(.RemoveAt(combinedRange.startIndex))
+        case (1, 1):
+            return .CollapsedTo(.ReplaceAt(combinedRange.startIndex, with: combinedElements[0]))
+        default:
+            return .CollapsedTo(.ReplaceRange(combinedRange, with: combinedElements))
+        }
+    }
+
+    func shift(delta: Int) -> ArrayModification<Element> {
+        switch self {
+        case .Insert(let e, at: let i):
+            return .Insert(e, at: i + delta)
+        case .RemoveAt(let i):
+            return .RemoveAt(i + delta)
+        case .ReplaceAt(let i, with: let e):
+            return .ReplaceAt(i + delta, with: e)
+        case .ReplaceRange(let range, with: let es):
+            return .ReplaceRange(range.startIndex + delta ..< range.endIndex + delta, with: es)
+        }
     }
 }
 
-extension Array {
-    mutating func apply(change: ArrayChange<Element>) {
-        switch change {
+internal enum ArrayModificationMergeResult<Element> {
+    case DisjunctOrderedBefore
+    case DisjunctOrderedAfter
+    case CollapsedToNoChange
+    case CollapsedTo(ArrayModification<Element>)
+}
+
+extension RangeReplaceableCollectionType where Index == Int {
+    internal mutating func apply(modification: ArrayModification<Generator.Element>) {
+        switch modification {
         case .Insert(let element, at: let index):
             self.insert(element, atIndex: index)
         case .RemoveAt(let index):
             self.removeAtIndex(index)
         case .ReplaceAt(let index, with: let element):
-            self[index] = element
+            self.replaceRange(Range(start: index, end: index + 1), with: [element])
         case .ReplaceRange(let range, with: let elements):
             self.replaceRange(range, with: elements)
-        case .ReplaceAll(let elements):
-            self = elements
+        }
+    }
+}
+
+//MARK: ArrayChange
+
+public struct ArrayChange<Element>: ChangeType {
+    public typealias Value = [Element]
+
+    internal var initialCount: Int
+    internal var finalCount: Int
+
+    /// List of independent modifications to apply, in order of startIndex.
+    /// All indices are understood to be in the *resulting* array.
+    /// (But if you apply them in order to the original, you'll get the correct result.)
+    internal var modifications: [ArrayModification<Element>] = []
+
+    internal init(initialCount: Int, finalCount: Int, modifications: [ArrayModification<Element>]) {
+        assert(finalCount == initialCount + modifications.reduce(0, combine: { c, m in c + m.countDelta }))
+        self.initialCount = initialCount
+        self.finalCount = finalCount
+        self.modifications = modifications
+    }
+
+    internal init(count: Int, modification: ArrayModification<Element>) {
+        self.initialCount = count
+        self.finalCount = count + modification.countDelta
+        self.modifications = [modification]
+    }
+    public init(from previousValue: Value, to newValue: Value) {
+        self.init(count: previousValue.count, modification: .ReplaceRange(0..<previousValue.count, with: newValue))
+    }
+
+    public var isNull: Bool { return modifications.isEmpty }
+
+    private mutating func addModification(new: ArrayModification<Element>) {
+        finalCount += new.countDelta
+        var pos = modifications.count - 1
+        var m = new
+        while pos >= 0 {
+            let old = modifications[pos]
+            let res = old.merge(m)
+            switch res {
+            case .DisjunctOrderedAfter:
+                modifications.insert(m, atIndex: pos + 1)
+                return
+            case .DisjunctOrderedBefore:
+                modifications[pos] = old.shift(m.countDelta)
+                pos -= 1
+                continue
+            case .CollapsedToNoChange:
+                modifications.removeAtIndex(pos)
+                return
+            case .CollapsedTo(let merged):
+                modifications.removeAtIndex(pos)
+                m = merged
+                pos -= 1
+                continue
+            }
+        }
+        modifications.insert(m, atIndex: 0)
+    }
+
+    public func applyOn(value: [Element]) -> [Element] {
+        precondition(value.count == initialCount)
+        var result = value
+        result.apply(self)
+        return result
+    }
+
+    public func merge(other: ArrayChange<Element>) -> ArrayChange<Element> {
+        precondition(finalCount == other.initialCount)
+        var result = self
+        for m in other.modifications {
+            result.addModification(m)
+        }
+        assert(result.finalCount == other.finalCount)
+        return result
+    }
+}
+
+extension RangeReplaceableCollectionType where Index == Int {
+    public mutating func apply(change: ArrayChange<Generator.Element>) {
+        for modification in change.modifications {
+            self.apply(modification)
         }
     }
 }
@@ -55,8 +209,8 @@ public protocol ArrayUpdatableType: UpdatableType, ArrayObservableType, RangeRep
 
 public final class ArrayVariable<Element>: ArrayUpdatableType, ArrayLiteralConvertible {
     public typealias Change = ArrayChange<Element>
-    public typealias SinkType = [Element]
-    public typealias ArrayElement = Element
+    public typealias ObservableValue = [Element]
+    public typealias SinkValue = [Element]
 
     private var _value: [Element]
     // These are created on demand and released immediately when unused
@@ -78,13 +232,13 @@ public final class ArrayVariable<Element>: ArrayUpdatableType, ArrayLiteralConve
     }
 
     /// The current value of this ArrayVariable.
-    public final var value: [Element] {
+    public var value: [Element] {
         get { return _value }
         set { setValue(newValue) }
     }
 
-    /// A source that reports all future values of this variable.
-    public final var futureChanges: Source<ArrayChange<Element>> {
+    /// A source that reports all future changes of this variable.
+    public var futureChanges: Source<ArrayChange<Element>> {
         if let futureChanges = _futureChanges {
             return futureChanges.source
         }
@@ -95,13 +249,11 @@ public final class ArrayVariable<Element>: ArrayUpdatableType, ArrayLiteralConve
         }
     }
 
-    /// Update the value of this variable, and send the new value to all sinks that are currently connected.
-    /// The sinks are only triggered if the value is not equal to the previous value, according to the equality test given in init.
-    public final func setValue(value: [Element]) {
+    public func setValue(value: [Element]) {
+        let oldCount = _value.count
         _value = value
-        _futureChanges?.send(.ReplaceAll(value))
+        _futureChanges?.send(ArrayChange(count: oldCount, modification: .ReplaceRange(0..<oldCount, with: value)))
     }
-
 
     public var count: Int {
         return value.count
@@ -115,22 +267,8 @@ public final class ArrayVariable<Element>: ArrayUpdatableType, ArrayLiteralConve
             var connection: Connection? = nil
             let signal = Signal<SimpleChange<Int>>(
                 didConnectFirstSink: { signal in
-                    var count = self.count
                     connection = self.futureChanges.connect { change in
-                        let oldCount = count
-                        switch change {
-                        case .Insert(_, at: _):
-                            count += 1
-                        case .RemoveAt(_):
-                            count -= 1
-                        case .ReplaceAt(_, with: _):
-                            break // Count doesn't change.
-                        case .ReplaceRange(let range, with: let elements):
-                            count += elements.count - range.count
-                        case .ReplaceAll(let elements):
-                            count = elements.count
-                        }
-                        signal.send(SimpleChange(oldValue: oldCount, newValue: count))
+                        signal.send(SimpleChange(change.finalCount))
                     }
                 },
                 didDisconnectLastSink: { signal in
@@ -183,7 +321,7 @@ extension ArrayVariable: MutableCollectionType {
         }
         set {
             value[index] = newValue
-            _futureChanges?.send(.ReplaceAt(index, with: newValue))
+            _futureChanges?.send(ArrayChange(count: self.count, modification: .ReplaceAt(index, with: newValue)))
         }
     }
 
@@ -192,16 +330,18 @@ extension ArrayVariable: MutableCollectionType {
             return value[bounds]
         }
         set {
+            let oldCount = count
             value[bounds] = newValue
-            _futureChanges?.send(.ReplaceRange(bounds, with: Array<Element>(newValue)))
+            _futureChanges?.send(ArrayChange(count: oldCount, modification: .ReplaceRange(bounds, with: Array<Element>(newValue))))
         }
     }
 }
 
 extension ArrayVariable: RangeReplaceableCollectionType {
     public func replaceRange<C : CollectionType where C.Generator.Element == Generator.Element>(subRange: Range<Int>, with newElements: C) {
+        let oldCount = count
         value.replaceRange(subRange, with: newElements)
-        _futureChanges?.send(.ReplaceRange(subRange, with: Array<Element>(newElements)))
+        _futureChanges?.send(ArrayChange(count: oldCount, modification: .ReplaceRange(subRange, with: Array<Element>(newElements))))
     }
 
     // These have default implementations in terms of replaceRange, but doing them by hand makes for better change reports.
@@ -212,38 +352,38 @@ extension ArrayVariable: RangeReplaceableCollectionType {
 
     public func insert(newElement: Element, at index: Int) {
         value.insert(newElement, atIndex: index)
-        _futureChanges?.send(.Insert(newElement, at: index))
+        _futureChanges?.send(ArrayChange(count: self.count - 1, modification: .Insert(newElement, at: index)))
     }
 
     public func removeAtIndex(index: Int) -> Element {
         let result = value.removeAtIndex(index)
-        _futureChanges?.send(.RemoveAt(index))
+        _futureChanges?.send(ArrayChange(count: self.count + 1, modification: .RemoveAt(index)))
         return result
     }
 
     public func removeFirst() -> Element {
         let result = value.removeFirst()
-        _futureChanges?.send(.RemoveAt(0))
+        _futureChanges?.send(ArrayChange(count: self.count + 1, modification: .RemoveAt(0)))
         return result
     }
 
     public func removeLast() -> Element {
         let result = value.removeLast()
-        _futureChanges?.send(.RemoveAt(self.count))
+        _futureChanges?.send(ArrayChange(count: self.count + 1, modification: .RemoveAt(self.count)))
         return result
     }
     
     public func popLast() -> Element? {
         guard let result = value.popLast() else { return nil }
-        _futureChanges?.send(.RemoveAt(self.count))
+        _futureChanges?.send(ArrayChange(count: self.count + 1, modification: .RemoveAt(self.count)))
         return result
     }
 
     public func removeAll() {
+        let count = value.count
         value.removeAll()
-        _futureChanges?.send(.ReplaceAll([]))
+        _futureChanges?.send(ArrayChange(count: count, modification: .ReplaceRange(0..<count, with: [])))
     }
-
 }
 
 
---
+++ Tests/ArrayVariableTests.swift
@@ -0,0 +1,224 @@
+//
+//  ArrayVariableTests.swift
+//  GlueKit
+//
+//  Created by Károly Lőrentey on 2015-12-09.
+//  Copyright © 2015 Károly Lőrentey. All rights reserved.
+//
+
+import XCTest
+@testable import GlueKit
+
+private func ==<T: Equatable>(a: ArrayModification<T>, b: ArrayModification<T>) -> Bool {
+    let ar = a.toRangeReplacement()
+    let br = b.toRangeReplacement()
+    return ar.range == br.range && ar.elements == br.elements
+}
+
+private func ==<T: Equatable>(a: ArrayModificationMergeResult<T>, b: ArrayModificationMergeResult<T>) -> Bool {
+    switch a {
+    case .DisjunctOrderedAfter:
+        if case .DisjunctOrderedAfter = b {
+            return true
+        }
+        return false
+    case .DisjunctOrderedBefore:
+        if case .DisjunctOrderedBefore = b {
+            return true
+        }
+        return false
+    case .CollapsedToNoChange:
+        if case .CollapsedToNoChange = b {
+            return true
+        }
+        return false
+    case .CollapsedTo(let ae):
+        if case .CollapsedTo(let be) = b {
+            return ae == be
+        }
+        return false
+    }
+}
+
+class ArrayModificationTests: XCTestCase {
+
+    func testInsertion() {
+        var a = [1, 2, 3]
+        let mod = ArrayModification.Insert(10, at: 2)
+
+        XCTAssertEqual(mod.countDelta, 1)
+        XCTAssertEqual(mod.toRangeReplacement().range, 2..<2)
+        XCTAssertEqual(mod.toRangeReplacement().elements, [10])
+
+        a.apply(mod)
+
+        XCTAssertEqual(a, [1, 2, 10, 3])
+    }
+
+    func testRemoval() {
+        var a = [1, 2, 3]
+        let mod = ArrayModification<Int>.RemoveAt(1)
+
+        XCTAssertEqual(mod.countDelta, -1)
+        XCTAssertEqual(mod.toRangeReplacement().range, 1..<2)
+        XCTAssertEqual(mod.toRangeReplacement().elements, [])
+
+        a.apply(mod)
+
+        XCTAssertEqual(a, [1, 3])
+    }
+
+    func testReplacement() {
+        var a = [1, 2, 3]
+        let mod = ArrayModification.ReplaceAt(1, with: 10)
+
+        XCTAssertEqual(mod.countDelta, 0)
+        XCTAssertEqual(mod.toRangeReplacement().range, 1..<2)
+        XCTAssertEqual(mod.toRangeReplacement().elements, [10])
+
+        a.apply(mod)
+
+        XCTAssertEqual(a, [1, 10, 3])
+    }
+
+    func testRangeReplacement() {
+        var a = [1, 2, 3]
+        let mod = ArrayModification.ReplaceRange(1...1, with: [10, 20])
+
+        XCTAssertEqual(mod.countDelta, 1)
+        XCTAssertEqual(mod.toRangeReplacement().range, 1..<2)
+        XCTAssertEqual(mod.toRangeReplacement().elements, [10, 20])
+
+        a.apply(mod)
+
+        XCTAssertEqual(a, [1, 10, 20, 3])
+    }
+
+    func testMergeIntoEmpty() {
+        let first = ArrayModification.ReplaceRange(10..<10, with: ["a", "b", "c"])
+
+        let m1 = ArrayModification<String>.ReplaceRange(10..<13, with: [])
+        XCTAssert(first.merge(m1) == ArrayModificationMergeResult.CollapsedToNoChange)
+    }
+
+    func testMergeIntoNonEmpty() {
+        let first = ArrayModification.ReplaceRange(10..<20, with: ["a", "b", "c"])
+        // final range of first: 10..<13
+
+        let m1 = ArrayModification.ReplaceRange(1..<5, with: ["1", "2"])
+        XCTAssert(first.merge(m1) == ArrayModificationMergeResult.DisjunctOrderedBefore)
+
+        let m2 = ArrayModification.ReplaceRange(5..<10, with: ["1", "2"])
+        XCTAssert(first.merge(m2) == ArrayModificationMergeResult.CollapsedTo(.ReplaceRange(5..<20, with: ["1", "2", "a", "b", "c"])))
+
+        let m3 = ArrayModification.ReplaceRange(5..<11, with: ["1", "2"])
+        XCTAssert(first.merge(m3) == ArrayModificationMergeResult.CollapsedTo(.ReplaceRange(5..<20, with: ["1", "2", "b", "c"])))
+
+        let m4 = ArrayModification.ReplaceRange(9..<15, with: ["1", "2"])
+        XCTAssert(first.merge(m4) == ArrayModificationMergeResult.CollapsedTo(.ReplaceRange(9..<22, with: ["1", "2"])))
+
+        let m5 = ArrayModification.ReplaceRange(10..<10, with: ["1", "2"])
+        XCTAssert(first.merge(m5) == ArrayModificationMergeResult.CollapsedTo(.ReplaceRange(10..<20, with: ["1", "2", "a", "b", "c"])))
+
+        let m6 = ArrayModification.ReplaceRange(10..<11, with: ["1", "2"])
+        XCTAssert(first.merge(m6) == ArrayModificationMergeResult.CollapsedTo(.ReplaceRange(10..<20, with: ["1", "2", "b", "c"])))
+
+        let m7 = ArrayModification.ReplaceRange(10..<13, with: ["1", "2"])
+        XCTAssert(first.merge(m7) == ArrayModificationMergeResult.CollapsedTo(.ReplaceRange(10..<20, with: ["1", "2"])))
+
+        let m8 = ArrayModification.ReplaceRange(10..<14, with: ["1", "2"])
+        XCTAssert(first.merge(m8) == ArrayModificationMergeResult.CollapsedTo(.ReplaceRange(10..<21, with: ["1", "2"])))
+
+        let m9 = ArrayModification.ReplaceRange(11..<12, with: ["1", "2"])
+        XCTAssert(first.merge(m9) == ArrayModificationMergeResult.CollapsedTo(.ReplaceRange(10..<20, with: ["a", "1", "2", "c"])))
+
+        let m10 = ArrayModification.ReplaceRange(11..<15, with: ["1", "2"])
+        XCTAssert(first.merge(m10) == ArrayModificationMergeResult.CollapsedTo(.ReplaceRange(10..<22, with: ["a", "1", "2"])))
+
+        let m11 = ArrayModification.ReplaceRange(11..<20, with: ["1", "2"])
+        XCTAssert(first.merge(m11) == ArrayModificationMergeResult.CollapsedTo(.ReplaceRange(10..<27, with: ["a", "1", "2"])))
+
+        let m12 = ArrayModification.ReplaceRange(13..<13, with: ["1", "2"])
+        XCTAssert(first.merge(m12) == ArrayModificationMergeResult.CollapsedTo(.ReplaceRange(10..<20, with: ["a", "b", "c", "1", "2"])))
+
+        let m13 = ArrayModification.ReplaceRange(13..<14, with: ["1", "2"])
+        XCTAssert(first.merge(m13) == ArrayModificationMergeResult.CollapsedTo(.ReplaceRange(10..<21, with: ["a", "b", "c", "1", "2"])))
+
+        let m14 = ArrayModification.ReplaceRange(14..<14, with: ["1", "2"])
+        XCTAssert(first.merge(m14) == ArrayModificationMergeResult.DisjunctOrderedAfter)
+
+        let m15 = ArrayModification.ReplaceRange(25...30, with: ["1", "2"])
+        XCTAssert(first.merge(m15) == ArrayModificationMergeResult.DisjunctOrderedAfter)
+    }
+}
+
+class ArrayChangeTests: XCTestCase {
+
+    func testExerciseMerging() {
+        // Exhaustively test the merging of all variations of modification sequences.
+        let startSequence = [0, 1]
+        let maxLevels = 4
+        let maxInsertionLength = 2
+
+        func insertionsAtLevel(level: Int) -> [[Int]] {
+            // Returns an array of [], [31], [31, 32], ..., up to maxInsertionLength
+            let s = 10 * level
+            return (1...maxInsertionLength).map { (1...$0).map { s + $0 } }
+        }
+
+        func printTrace(input: [Int], change: ArrayChange<Int>, output: [Int], trace: [ArrayModification<Int>], applied: [Int]) {
+            print("Given this input:")
+            print("    \(input)")
+            print("This sequence of changes:")
+            for t in trace {
+                let r = t.toRangeReplacement()
+                print("    Replace \(r.range) with \(r.elements)")
+            }
+            print("Was collapsed to:")
+            for m in change.modifications {
+                let r = m.toRangeReplacement()
+                print("    Replace \(r.range) with \(r.elements)")
+            }
+            print("Which resulted in:")
+            print("     \(applied)")
+            print("Instead of:")
+            print("     \(output)")
+        }
+
+        func recurse(level: Int, input: [Int], change: ArrayChange<Int>, output: [Int], trace: [ArrayModification<Int>]) {
+            let applied = change.applyOn(input)
+            if applied != output {
+                XCTAssertEqual(applied, output)
+                printTrace(input, change: change, output: output, trace: trace, applied: applied)
+                return
+            }
+
+            if level < maxLevels {
+                for startIndex in output.startIndex...output.endIndex {
+                    for endIndex in startIndex...output.endIndex {
+                        for insertion in insertionsAtLevel(level) {
+                            if insertion.count == 0 && endIndex - startIndex == 0 {
+                                // Skip replacing empty range to empty array
+                                continue
+                            }
+                            var nextOutput = output
+                            nextOutput.replaceRange(startIndex..<endIndex, with: insertion)
+                            let mod = ArrayModification.ReplaceRange(startIndex..<endIndex, with: insertion)
+                            let nextChange = change.merge(ArrayChange(count: output.count, modification: mod))
+                            recurse(level + 1, input: input, change: nextChange, output: nextOutput, trace: trace + [mod])
+                        }
+                    }
+                }
+            }
+        }
+
+        let startChange = ArrayChange<Int>(initialCount: startSequence.count, finalCount: startSequence.count, modifications: [])
+        recurse(1, input: startSequence, change: startChange, output: startSequence, trace: [])
+
+    }
+}
+
+
+class ArrayVariableTests: XCTestCase {
+
+}
