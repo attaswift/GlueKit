d1fac430d5606c95c83101313b1b96034cfc25bf
Karoly Lorentey <karoly@lorentey.hu>
2015-12-11 16:33:24 +0100

    Add SampleModel project and Documentation folder.

---
+++ SampleModel/SampleModel.h
@@ -0,0 +1,19 @@
+//
+//  SampleModel.h
+//  SampleModel
+//
+//  Created by Károly Lőrentey on 2015-12-11.
+//  Copyright © 2015 Károly Lőrentey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleModel.
+FOUNDATION_EXPORT double SampleModelVersionNumber;
+
+//! Project version string for SampleModel.
+FOUNDATION_EXPORT const unsigned char SampleModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleModel/PublicHeader.h>
+
+
