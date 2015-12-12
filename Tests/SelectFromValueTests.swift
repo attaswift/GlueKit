8cbde17c44f4312f619923b5d7fed233faaa4b7f
Karoly Lorentey <karoly@lorentey.hu>
2015-12-12 15:12:08 +0100

    Rename SelectTests to SelectFromValueTests.

--- Tests/SelectFromValueTests.swift
+++ Tests/SelectFromValueTests.swift
@@ -9,39 +9,183 @@
 import XCTest
 import GlueKit
 
-private class Company {
+private class Entity {
+    let int = Variable<Int>(0)
+    let floats = ArrayVariable<Float>()
+
+    init(_ int: Int, _ floats: [Float] = []) {
+        self.int.value = int
+        self.floats.value = floats
+    }
+}
+
+class SelectFromValuesTests: XCTestCase {
+    func testQueryAndUpdateSingleValue() {
+        let e = Entity(42)
+        let v = Variable<Entity>(e)
+        let path = v.select{$0.int}
+
+        XCTAssertEqual(path.value, 42)
+
+        path.value = 23
+
+        XCTAssertEqual(path.value, 23)
+        XCTAssertEqual(e.int.value, 23)
+    }
+
+    func testObservingSingleValueChanges() {
+        let e1 = Entity(1)
+        let e2 = Entity(100)
+        let v = Variable<Entity>(e1)
+        let path = v.select{$0.int}
+
+        var r = [Int]()
+        let c = path.futureValues.connect { r.append($0) }
+
+        e1.int.value = 2
+
+        XCTAssertEqual(r, [2])
+
+        path.value = 3
+
+        XCTAssertEqual(r, [2, 3])
+
+        v.value = e2
+
+        XCTAssertEqual(r, [2, 3, 100])
+
+        c.disconnect()
+    }
+
+    func testQueryAndUpdateArrayValue() {
+        let e1 = Entity(0, [0.0])
+        let e2 = Entity(2, [100.0])
+        let v = Variable<Entity>(e1)
+        let path: UpdatableArray<Float> = v.select{$0.floats}
+
+        XCTAssertEqual(path.value, [0.0])
+
+        path.value = [1.0, 2.0]
+
+        XCTAssertEqual(path.value, [1.0, 2.0])
+        XCTAssertEqual(e1.floats.value, [1.0, 2.0])
+
+        path.removeAtIndex(1)
+
+        let foo = ArrayVariable<Int>()
+        foo.insert(1, at: 0)
+        foo.insert(2, at: 1)
+        foo.removeAtIndex(0)
+
+        let upd = foo.updatableArray
+        upd.insert(3, atIndex: 0)
+        upd.removeAtIndex(0)
+
+        XCTAssertEqual(path.value, [1.0])
+        XCTAssertEqual(e1.floats.value, [1.0])
+
+        v.value = e2
+
+        XCTAssertEqual(path.value, [100.0])
+    }
+
+    func testObservingArrayChanges() {
+        let e1 = Entity(1)
+        let e2 = Entity(100)
+        let v = Variable<Entity>(e1)
+        let path = v.select{$0.int}
+
+        var r = [Int]()
+        let c = path.futureValues.connect { r.append($0) }
+
+        e1.int.value = 2
+
+        XCTAssertEqual(r, [2])
+
+        path.value = 3
+
+        XCTAssertEqual(r, [2, 3])
+
+        v.value = e2
+        
+        XCTAssertEqual(r, [2, 3, 100])
+        
+        c.disconnect()
+    }
+
+}
+
+private class Company: CustomStringConvertible {
     let name = Variable<String>("")
     let employees = ArrayVariable<Person>([])
+    let apps = ArrayVariable<App>([])
 
     init(name: String) {
         self.name.value = name
     }
+
+    var description: String { return self.name.value }
+
+    func fire(employee: Person) {
+        if let index = employees.indexOf({ $0 === self }) {
+            employees.removeAtIndex(index)
+            print("\(employee) has been fired from \(self).")
+        }
+    }
+
+    func sell(app: App, to buyer: Company) {
+        if let index = apps.indexOf({ $0 === app }) {
+            print("\(self) has sold \(app) to \(buyer)")
+            app.developer.value = buyer
+            buyer.apps.append(app)
+            apps.removeAtIndex(index)
+        }
+    }
 }
 
-private class App {
+private class App: CustomStringConvertible {
     let name = Variable<String>("")
     let version = Variable<String>("")
-    let developer: Variable<Company>
+    let developer: UnownedVariable<Company>
 
     init(name: String, version: String, developer: Company) {
         self.name.value = name
         self.version.value = version
-        self.developer = Variable(developer)
+        self.developer = UnownedVariable(developer)
+
+        developer.apps.append(self)
     }
+
+    var description: String { return self.name.value }
 }
 
-private class Person {
+private class Person: CustomStringConvertible {
     let name = Variable<String>("")
     let favoriteApp: Variable<App>
+    let employer: WeakVariable<Company>
 
-    init(name: String, favoriteApp: App) {
+    init(name: String, favoriteApp: App, employer: Company?) {
         self.name.value = name
         self.favoriteApp = Variable(favoriteApp)
+        self.employer = WeakVariable(employer)
+
+        employer?.employees.append(self)
+    }
+
+    var description: String { return self.name.value }
+
+    func quit() {
+        if let employer = self.employer.value {
+            if let index = employer.employees.indexOf({ $0 === self }) {
+                employer.employees.removeAtIndex(index)
+            }
+            self.employer.value = nil
+            print("\(self) has quit from \(employer).")
+        }
     }
 }
 
-
-class SelectOperatorTests: XCTestCase {
+class SelectFromValueTestsExamples: XCTestCase {
     private struct Companies {
         let appNinja: Company
         let banditware: Company
@@ -74,11 +218,11 @@ class SelectOperatorTests: XCTestCase {
             baconCrush: App(name: "Bacon Crush", version: "3.2.0", developer: companies.appNinja),
             cowLicker: App(name: "Cow Licker", version: "1.0.0", developer: companies.coolSoftware))
         people = People(
-            adam: Person(name: "Adam Edgar Whatever", favoriteApp: apps.angryWombats),
-            ben: Person(name: "Benjamin Smith", favoriteApp: apps.angryWombats),
-            cecil: Person(name: "Cecil Testperson", favoriteApp: apps.angryWombats),
-            david: Person(name: "David Sampleguy", favoriteApp: apps.angryWombats),
-            emily: Person(name: "Emily von Fixture", favoriteApp: apps.cowLicker))
+            adam: Person(name: "Adam Edgar Whatever", favoriteApp: apps.angryWombats, employer: companies.coolSoftware),
+            ben: Person(name: "Benjamin Smith", favoriteApp: apps.angryWombats, employer: companies.appNinja),
+            cecil: Person(name: "Cecil Testperson", favoriteApp: apps.angryWombats, employer: companies.appNinja),
+            david: Person(name: "David Sampleguy", favoriteApp: apps.angryWombats, employer: companies.coolSoftware),
+            emily: Person(name: "Emily von Fixture", favoriteApp: apps.cowLicker, employer: companies.banditware))
 
         companies.appNinja.employees.value = [people.ben, people.cecil]
         companies.banditware.employees.value = [people.emily]
