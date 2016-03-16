c258235a2ebb9b448be73fa2272ec463011617d8
Karoly Lorentey <karoly@lorentey.hu>
2016-03-16 23:23:00 +0100

    Add UIGestureRecognizer.recognizedSource.

---
+++ Sources/UIGestureRecognizer Extensions.swift
@@ -0,0 +1,38 @@
+//
+//  UIGestureRecognizer Extensions.swift
+//  GlueKit
+//
+//  Created by Károly Lőrentey on 2016-03-16.
+//  Copyright © 2016 Károly Lőrentey. All rights reserved.
+//
+
+import UIKit
+
+var associatedObjectKey: Void = ()
+
+extension UIGestureRecognizer {
+    public class func create() -> Self {
+        let target = GestureRecognizerTarget()
+        let result = self.init(target: target, action: #selector(GestureRecognizerTarget.gestureRecognizerDidFire))
+        objc_setAssociatedObject(self, &associatedObjectKey, target, .OBJC_ASSOCIATION_RETAIN)
+        return result
+    }
+
+    public var recognizedSource: Source<Void> {
+        if let target = objc_getAssociatedObject(self, &associatedObjectKey) as? GestureRecognizerTarget {
+            return target.signal.source
+        }
+        let target = GestureRecognizerTarget()
+        self.addTarget(target, action: #selector(GestureRecognizerTarget.gestureRecognizerDidFire))
+        objc_setAssociatedObject(self, &associatedObjectKey, target, .OBJC_ASSOCIATION_RETAIN)
+        return target.signal.source
+    }
+}
+
+private class GestureRecognizerTarget: NSObject {
+    var signal = LazySignal<Void>()
+
+    @objc func gestureRecognizerDidFire() {
+        signal.send()
+    }
+}
