39374d46f66340920233046cb4b7b8a3f5aeea06
Karoly Lorentey <karoly@lorentey.hu>
2016-11-02 16:37:56 +0100

    Add tests for BufferedValue.swift

--- Tests/BufferedValueTests.swift
+++ Tests/BufferedValueTests.swift
@@ -21,9 +21,6 @@ class BufferedValueTests: XCTestCase {
         withExtendedLifetime(buffered) {}
     }
 
-    func foo(_ observable: TestObservableValue<Int>, _ weakSink: inout MockSink<Int>?) {
-    }
-
     func test_isntRetainedByObservable() {
         let observable = TestObservableValue(0)
         weak var weakSink: MockValueUpdateSink<Int>? = nil
@@ -36,5 +33,38 @@ class BufferedValueTests: XCTestCase {
         }
         // If the sink is still alive, the buffered observable wasn't deallocated.
         XCTAssertNil(weakSink, "Possible retain cycle")
+    }
+
+    func test_updates() {
+        let observable = TestObservableValue(0)
+        let buffered = observable.buffered()
+
+        XCTAssertEqual(buffered.value, 0)
+        observable.value = 1
+        XCTAssertEqual(buffered.value, 1)
+
+        let sink = MockValueUpdateSink(buffered)
+
+        sink.expecting(["begin", "1 -> 2", "end"]) {
+            observable.value = 2
+        }
+
+        sink.expecting("begin") {
+            observable.begin()
+        }
+
+        sink.expectingNothing {
+            observable.value = 3
+        }
+
+        sink.expectingNothing {
+            observable.value = 4
+        }
+
+        sink.expecting(["2 -> 4", "end"]) {
+            observable.end()
+        }
+
+        sink.disconnect()
     }
 }
