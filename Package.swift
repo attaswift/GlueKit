d7c9552a9a125990305b32270796c487de383b3d
Karoly Lorentey <karoly@lorentey.hu>
2016-11-09 00:19:35 +0100

    Describe test dependencies in package description.

--- Package.swift
+++ Package.swift
@@ -2,5 +2,12 @@ import PackageDescription
 
 let package = Package(
     name: "GlueKit",
-    dependencies: [.Package(url: "https://github.com/lorentey/BTree", majorVersion: 4, minor: 0)]
+    targets: [
+        // Test targets
+        Target(name: "GlueKitTests", dependencies: ["GlueKit"]),
+        Target(name: "PerformanceTests", dependencies: ["GlueKit"]),
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/lorentey/BTree", majorVersion: 4, minor: 0)
+    ]
 )
