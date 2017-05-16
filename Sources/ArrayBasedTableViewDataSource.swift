b208422bb89fc49e45e6d29ea42de304dd15e7b0
Karoly Lorentey <karoly@lorentey.hu>
2017-05-16 11:11:58 +0200

    Improve ArrayBasedTableViewDataSource (experimental)

--- Sources/ArrayBasedTableViewDataSource.swift
+++ Sources/ArrayBasedTableViewDataSource.swift
@@ -15,43 +15,59 @@ extension IndexSet {
     }
 }
 
+public func <-- <Element, Source: ObservableArrayType>(target: DependentArray<Element>, source: Source) where Source.Element == Element {
+    target.origin = source.anyObservableArray
+}
+
+public class DependentArray<Element> {
+    private var _itemsRef = Variable<AnyObservableArray<Element>>(.constant([]))
+    private var _items: AnyObservableArray<Element>
+    public let changes: AnySource<ArrayChange<Element>>
+
+    public init() {
+        self._items = _itemsRef.unpacked()
+        self.changes = _items.changes
+    }
+
+    public var origin: AnyObservableArray<Element> {
+        get {
+            return _items
+        }
+        set {
+            _itemsRef.value = newValue
+        }
+    }
+}
+
 public class ArrayBasedTableViewDataSource<Item: Hashable, Cell: UITableViewCell>: NSObject, UITableViewDataSource {
     public let tableView: UITableView
     public let reuseIdentifier: String
-    public let cellLoader: (Item, Cell) -> Void
+    public let cellLoader: (Cell, Item) -> Void
 
-    private var _itemsRef = Variable<AnyObservableArray<Item>>(.constant([]))
-    private var _items: AnyObservableArray<Item>
-    private var _changes: AnySource<ArrayChange<Item>>
+    public let items = DependentArray<Item>()
 
-    public var items: AnyObservableArray<Item> {
-        get { return _itemsRef.value }
-        set { _itemsRef.value = newValue }
-    }
-
-    private struct Sink<Item: Hashable, Cell: UITableViewCell>: UniqueOwnedSink {
+    private struct Sink: UniqueOwnedSink {
         typealias Owner = ArrayBasedTableViewDataSource<Item, Cell>
         unowned(unsafe) let owner: Owner
         func receive(_ change: ArrayChange<Item>) { owner.receive(change) }
     }
 
-    public init(tableView: UITableView, reuseIdentifier: String, cellLoader: @escaping (Item, Cell) -> Void) {
+    public init(tableView: UITableView, reuseIdentifier: String, cellLoader: @escaping (Cell, Item) -> Void) {
         self.tableView = tableView
         self.reuseIdentifier = reuseIdentifier
         self.cellLoader = cellLoader
-        self._items = _itemsRef.unpacked()
-        self._changes = _items.changes
         super.init()
 
-        self._changes.add(Sink<Item, Cell>(owner: self))
+        self.items.changes.add(Sink(owner: self))
+        tableView.dataSource = self
     }
 
     deinit {
-        self._changes.remove(Sink<Item, Cell>(owner: self))
+        self.items.changes.remove(Sink(owner: self))
     }
 
     public convenience init(tableView: UITableView, reuseIdentifier: String) {
-        self.init(tableView: tableView, reuseIdentifier: reuseIdentifier, cellLoader: { item, cell in
+        self.init(tableView: tableView, reuseIdentifier: reuseIdentifier, cellLoader: { cell, item in
             cell.textLabel!.text = "\(item)"
         })
     }
@@ -59,8 +75,8 @@ public class ArrayBasedTableViewDataSource<Item: Hashable, Cell: UITableViewCell
     private func receive(_ change: ArrayChange<Item>) {
         guard !change.isEmpty else { return }
         let batch = change.separated()
-        print("Batch: count=\(change.initialCount)/\(change.finalCount), deleted=\(Array(batch.deleted)), inserted=\(Array(batch.inserted)), moved=\(batch.moved)")
-        assert(batch.change.finalCount == items.count)
+        //print("Batch: count=\(change.initialCount)/\(change.finalCount), deleted=\(Array(batch.deleted)), inserted=\(Array(batch.inserted)), moved=\(batch.moved)")
+        assert(batch.change.finalCount == items.origin.count)
 
         tableView.beginUpdates()
         tableView.deleteRows(at: batch.deleted.toIndexPaths(), with: .fade)
@@ -68,7 +84,7 @@ public class ArrayBasedTableViewDataSource<Item: Hashable, Cell: UITableViewCell
         for (from, to) in batch.moved {
             tableView.moveRow(at: IndexPath(row: from, section: 0), to: IndexPath(row: to, section: 0))
             if let cell = tableView.cellForRow(at: IndexPath(row: from, section: 0)) as? Cell {
-                cellLoader(items[to], cell)
+                cellLoader(cell, items.origin[to])
             }
         }
         tableView.endUpdates()
@@ -80,13 +96,13 @@ public class ArrayBasedTableViewDataSource<Item: Hashable, Cell: UITableViewCell
 
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         guard section == 0 else { return 0 }
-        return items.count
+        return items.origin.count
     }
 
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         precondition(indexPath.section == 0)
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! Cell
-        cellLoader(items[indexPath.row], cell)
+        cellLoader(cell, items.origin[indexPath.row])
         return cell
     }
 
